da2c91e67401b32c4f736330ec7a5fde9b9cae99
TemporalFuzz <vanxmax@gmail.com>
2017-07-21 09:36:21 -0700

    Update circle.js
    Added sleep functionality for realism. This will probably need some adjustments in the future.

--- scripts/circle.js
+++ scripts/circle.js
@@ -1,28 +1,36 @@
 var Circle = function(config) {
-    /** Position and Velocity **/
-    this.pos = config.pos || [config.x, config.y];
-    this.vel = config.vel || [0, 0];
-    
-    /** Bounciness **/
-    this.bcf = config.bcf || 0.75;
-    
-    /** Radius **/
-    this.rad = config.rad || 15;
-    
-    /** Array of Constraints **/
-    this.constraints = [];
+  /** Position and Velocity **/
+  this.pos = config.pos || [config.x, config.y];
+  this.vel = config.vel || [0, 0];
+  
+  /** Bounciness **/
+  this.bcf = config.bcf || 0.75;
+  
+  /** Radius **/
+  this.rad = config.rad || 15;
+  
+  /** Array of Constraints **/
+  this.constraints = [];
+  
+  /** Physical Sleep **/
+  this.asleep = false;
 };
 Circle.prototype.update = function() {
+  if(vectMag(this.vel) > 3 * 0.8) {
+    this.asleep = false;
+  }
+  if(!this.asleep) {
     this.pos = vectAdd(this.vel, this.pos);//Add velocity to position
-    
+  
     this.vel[1] += 0.5;//Gravity
-    this.vel = vectMult(this.vel, 0.99);//Air resistance
+  }
+  this.vel = vectMult(this.vel, 0.99);//Air resistance
 };
 Circle.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.fillStyle = "#FF0000";
-    ctx.ellipse(this.pos[0], this.pos[1], this.rad, this.rad, 0, 0, 6.279);
-    ctx.fill();
+  ctx.beginPath();
+  ctx.fillStyle = "#FF0000";
+  ctx.ellipse(this.pos[0], this.pos[1], this.rad, this.rad, 0, 0, 6.279);
+  ctx.fill();
 };
 Circle.prototype.moveTowards = function(target, force) {
   this.vel = vectAdd(this.vel, vectMult(vectNorm(vectSub(target, this.pos)), force));
@@ -33,6 +41,13 @@ Circle.prototype.display = function() {
   }
   this.update();
   this.draw();
+};
+Circle.prototype.trySleep = function() {
+  if(vectMag(this.vel) < 3) {
+    console.log("heck");
+    this.asleep = true;
+    this.vel = vectMult(this.vel, 0.8);
+  }
 };
 Circle.prototype.collideLine = function(line) {
   if(line.one[0] === line.two[0] ||
@@ -49,8 +64,8 @@ Circle.prototype.collideLine = function(line) {
       this.vel = vectMult(vectSub(n, vectRefl(vectSub(n, this.vel), n, vectAdd(n, [1, PM(line.one, line.two)]))), -line.bcf * this.bcf);
       this.pos = vectAdd(n, vectMult(vectNorm([1, PM(line.one, line.two)]), (this.rad + line.rad) * (this.pos[1] > n[1] ? -1 : 1) * (line.one[0] > line.two[0] ? -1 : 1) * (line.one[1] > line.two[1] ? -1 : 1)));
     }
+    this.trySleep();
   }
 };
-
 
 
