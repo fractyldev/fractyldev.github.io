41f35d4517dee43daacfbb7027133dad5cc721f8
TemporalFuzz <vanxmax@gmail.com>
2017-07-22 20:57:57 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 var circles = [], lines = [], constraints = [];
 
+var selected;
+
 function update() {
   ctx.lineCap = "round";
   ctx.fillStyle = "#FFFFFF";
@@ -20,62 +22,87 @@ function update() {
   for(var i = 0; i < constraints.length; i++) {
     constraints[i].display();
   }
+  
+  drag();
 };
 
 function onLoad() {
   circles.push(new Circle({
+    x: 100,
+    y: 25,
+    rad: 25,
+    bcf: 0.95,
+    mass: 2
+  }));
+  
+  circles.push(new Circle({
     x: 200,
     y: 50,
-    rad: 50,
+    rad: 15,
     bcf: 0.95,
+    mass: 1
   }));
   
   circles.push(new Circle({
-    x: 400,
-    y: 100,
-    rad: 30,
+    x: 150,
+    y: 150,
+    rad: 15,
     bcf: 0.95,
+    mass: 1
   }));
   
-  circles.push(new Circle({
-    x: 300,
-    y: 300,
-    rad: 30,
-    bcf: 0.95,
-  }));
-  
-  constraints.push(new Cord({
+  constraints.push(new Rod({
     one: circles[0],
     two: circles[1],
-    length: 220,
-    rigidity: 30,
+    length: 110,
+    rigidity: 15,
   }));
-  constraints.push(new Cord({
+  constraints.push(new Rod({
     one: circles[0],
     two: circles[2],
-    length: 220,
-    rigidity: 30,
+    length: 110,
+    rigidity: 15,
   }));
-  constraints.push(new Cord({
+  constraints.push(new Rod({
     one: circles[1],
     two: circles[2],
-    length: 220,
-    rigidity: 30
+    length: 110,
+    rigidity: 15
   }));
   
   lines.push(new Line({
-    one: [100, 300],
-    two: [300, 400],
+    one: [50, 150],
+    two: [150, 200],
     bcf: 0.95,
   }));
   lines.push(new Line({
-    one: [500, 350],
-    two: [300, 400],
+    one: [250, 175],
+    two: [150, 200],
     bcf: 0.95,
   }));
   
   setInterval(update, 16);
 }
+
+function drag() {
+  if(mouseIsPressed && !selected) {
+    for(var i = 0; i < circles.length; i++) {
+      if(vectDist([mouseX * 600/canvas.width, mouseY * 500/canvas.height], circles[i].pos) < circles[i].rad * 2) {
+        selected = circles[i];
+      }
+    }
+  }
+  else {
+    selected = undefined;
+  }
+  
+  if(selected) {
+    selected.moveTowards([mouseX * 600/canvas.width, mouseY * 500/canvas.height], 10);
+  }
+}
+
+document.addEventListener("mousemove", mouseUpdate);
+
 
 document.addEventListener("DOMContentLoaded", onLoad);
 
